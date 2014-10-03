fdf628caecabe1f61880587e3970ffc463dae31f
Lee Barney <barney.cit@gmail.com>
2014-10-03 09:51:59 -0600

    first commit of fraction_choices2

---
+++ fraction_choices2/fraction_choices2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fraction_choices2
+//
+//  Created by Lee Barney on 10/2/14.
+//  Copyright (c) 2014 Lee Barney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ fraction_choices2/fraction_choices2/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  fraction_choices2
+//
+//  Created by Lee Barney on 10/2/14.
+//  Copyright (c) 2014 Lee Barney. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *showIsFractionalLabel;
+- (IBAction)checkFractional:(id)sender;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)checkFractional:(id)sender {
+    double aNumber = .25;
+    if( (aNumber > 0 && aNumber < 1) || (aNumber < 0 && aNumber > -1)){
+        _showIsFractionalLabel.text = [NSString stringWithFormat:@"%f is only fractional.", aNumber];
+    }
+    else{
+        _showIsFractionalLabel.text = [NSString stringWithFormat:@"%f has a whole number component.", aNumber];
+    }
+}
+@end
